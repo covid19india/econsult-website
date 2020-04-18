2b1e7994986915d19cfbea5a44a78c118ee83fb4
Debosmit Majumder <debmaj2programmania@rediffmail.com>
2020-04-19 03:54:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,50 +87,54 @@ function App() {
 
       
       <footer className="fadeInUp" style={{animationDelay: '2s'}}>
-
-        <h5>We stand with everyone fighting on the frontlines</h5>
-        <div className="link">
-          <a
-            href="https://github.com/covid19india"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            covid19india
-          </a>
-        </div>
+      <h5>We stand with everyone fighting on the frontlines</h5>
+      <div className="link">
         <a
-          href="https://github.com/covid19india/econsult-website"
-          className="button github"
+          href="https://github.com/covid19india"
           target="_blank"
           rel="noopener noreferrer"
-          style={{justifyContent: 'center'}}
         >
-        <a
-          href="https://twitter.com/covid19indiaorg"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button twitter"
-          style={{justifyContent: 'center'}}
-        >
-          <Icon.Twitter />
-          <span>View updates on Twitter</span>
+          covid19india
         </a>
-        <a
-          href="https://bit.ly/covid19crowd"
-          className="button telegram"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{justifyContent: 'center'}}
-          
-        >
-          <Icon.MessageCircle />
-          <span>Join Telegram to Collaborate!</span>
-        </a>
-        
-          <Icon.GitHub />
-          <span>Open Sourced on GitHub</span>
-        </a>
-      </footer>
+      </div>
+      <a
+        href="https://github.com/covid19india/econsult-website"
+        className="button github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon.GitHub />
+        <span>Open Sourced on GitHub</span>
+      </a>
+      <a
+        className="button excel"
+        href="http://patientdb.covid19india.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon.Database />
+        <span>Crowdsourced Patient Database&nbsp;</span>
+      </a>
+      <a
+        href="https://twitter.com/covid19indiaorg"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="button twitter"
+        style={{justifyContent: 'center'}}
+      >
+        <Icon.Twitter />
+        <span>View updates on Twitter</span>
+      </a>
+      <a
+        href="https://bit.ly/covid19crowd"
+        className="button telegram"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon.MessageCircle />
+        <span>Join Telegram to Collaborate!</span>
+      </a>
+    </footer>
     </div>
   );
 }
