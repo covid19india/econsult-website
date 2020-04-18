ff1e5228795a290cc766f1c8eaafd1c77ef5d09f
Debosmit Majumder <debmaj2programmania@rediffmail.com>
2020-04-19 05:17:07 +0530

    Moved Footer to Compnents

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import FAQ from './components/faq';
 import TNC from './components/tnc';
 import StartConsultation from './components/startConsultation';
 import TawkTo from './components/tawkto';
-
+import Footer from './components/footer'
 const history = require('history').createBrowserHistory;
 
 function App() {
@@ -84,48 +84,7 @@ function App() {
           )}
         /> 
       </Router>
-
-      
-      <footer className="fadeInUp" style={{animationDelay: '2s'}}>
-      <h5>We stand with everyone fighting on the frontlines</h5>
-      <div className="link">
-        <a
-          href="https://github.com/covid19india"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          covid19india
-        </a>
-      </div>
-      <a
-        href="https://github.com/covid19india/econsult-website"
-        className="button github"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon.GitHub />
-        <span>Open Sourced on GitHub</span>
-      </a>
-      <a
-        href="https://twitter.com/covid19indiaorg"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button twitter"
-        style={{justifyContent: 'center'}}
-      >
-        <Icon.Twitter />
-        <span>View updates on Twitter</span>
-      </a>
-      <a
-        href="https://bit.ly/covid19crowd"
-        className="button telegram"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon.MessageCircle />
-        <span>Join Telegram to Collaborate!</span>
-      </a>
-    </footer>
+      <Footer />
     </div>
   );
 }
